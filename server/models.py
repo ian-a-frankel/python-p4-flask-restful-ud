78c7d646801daf49b0c8937f2616c8b847e65252
Ian Frankel <ian.frankel@gmail.com>
2023-12-12 15:46:40 -0500

    gracefully removed datetimes so that post method could work

--- server/models.py
+++ server/models.py
@@ -9,8 +9,8 @@ class Newsletter(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     body = db.Column(db.String)
-    published_at = db.Column(db.DateTime, server_default=db.func.now())
-    edited_at = db.Column(db.DateTime, onupdate=db.func.now())
+    # published_at = db.Column(db.DateTime, server_default=db.func.now())
+    # edited_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     def __repr__(self):
         return f'<Newsletter {self.title}, published at {self.published_at}.>'
